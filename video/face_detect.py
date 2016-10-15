3b0fa31b3537887bb247cdf2edbc8f86b27ca462
David-Guo <guodw3@mail2.sysu.edu.cn>
2016-10-15 18:30:25 +0800

    complete

--- video/face_detect.py
+++ video/face_detect.py
@@ -1,30 +1,50 @@
 import numpy as np
 import cv2
 import time
+import requests
+import json
 
-cap = cv2.VideoCapture(0)
+#IP = "127.0.0.1"
+IP = "121.201.24.49"
+
+cap = cv2.VideoCapture(1)
 classfier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 color = (0, 0, 0)
 
 def uploadPic():
     pass
 
-while True:
-    success, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.equalizeHist(gray, gray)
-    faceRects = classfier.detectMultiScale(gray, 1.3, 5)
-    if len(faceRects) > 0:
-        for faceRect in faceRects:
-                x, y, w, h = faceRect
-                #cv2.rectangle(frame, (x, y), (x+w, y+h), color)
-                cv2.circle(frame, (x+w/2, y+h/2), w/2, color, 2, 8, 0)
-        cv2.imwrite("face.jpg", frame)
-        uploadPic()
-    cv2.imshow("face", frame)
-    key = cv2.waitKey(10)
-    c = chr(key & 255)
-    if c in ['q', 'Q', chr(27)]:
-        break
-    time.sleep(1)
-cv2.destroyAllWindows
+def main():
+    while True:
+        success, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.equalizeHist(gray, gray)
+        faceRects = classfier.detectMultiScale(gray, 1.3, 5)
+        if len(faceRects) > 0:
+            for faceRect in faceRects:
+                    x, y, w, h = faceRect
+                    #cv2.rectangle(frame, (x, y), (x+w, y+h), color)
+                    cv2.circle(frame, (x+w/2, y+h/2), w/2, color, 2, 8, 0)
+            cv2.imwrite("face.jpg", frame)
+
+            files = {'image': open("face.jpg", "rb")}
+            resp = requests.post(
+                    "http://" + IP + ":5000/upload_image", files=files)
+            json_resp = json.load(resp)
+            emotion = json_resp['emotion']
+            print emotion
+
+
+            uploadPic()
+        cv2.imshow("face", frame)
+        key = cv2.waitKey(10)
+        c = chr(key & 255)
+        if c in ['q', 'Q', chr(27)]:
+            break
+        time.sleep(1)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
+
